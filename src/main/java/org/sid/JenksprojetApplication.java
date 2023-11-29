af55a8d4ce9590126c7b904afc2d107320f9a32c
wiltord911 <douanlawiltord06@gmail.com>
2023-11-29 08:34:15 +0100

    mon deuxiemme commit

--- src/main/java/org/sid/JenksprojetApplication.java
+++ src/main/java/org/sid/JenksprojetApplication.java
@@ -7,7 +7,7 @@ package org.sid;
 public class JenksprojetApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello tout le monde !!!!!!!!");
+		System.out.println("hello tout le monde !!");
 		//SpringApplication.run(JenksprojetApplication.class, args);
 	}
 
